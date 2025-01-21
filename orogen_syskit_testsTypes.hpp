dadad73d33a9c532a95d70f4b6398b85ca439b7c
kapeps <rodrigo.kappes@tidewise.io>
2025-01-21 14:40:09 -0300

    feat: include test type for usage in tools/syskit

---
+++ orogen_syskit_testsTypes.hpp
@@ -0,0 +1,18 @@
+#ifndef orogen_syskit_tests_TYPES_HPP
+#define orogen_syskit_tests_TYPES_HPP
+
+namespace orogen_syskit_tests {
+    struct Time {
+        int microseconds = 0;
+        int tv_sec = 0;
+        int tv_usec = 0;
+    };
+
+    struct TestType {
+        /** @meta role logical_time */
+        Time timestamp;
+        int test_value = 0;
+    };
+}
+
+#endif
