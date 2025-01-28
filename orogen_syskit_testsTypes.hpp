fa0adc54e9442e8662329c8621bb4c54e71c6340
kapeps <rodrigo.kappes@tidewise.io>
2025-01-28 17:38:44 -0300

    fix: use base time for test type

--- orogen_syskit_testsTypes.hpp
+++ orogen_syskit_testsTypes.hpp
@@ -1,19 +1,12 @@
 #ifndef orogen_syskit_tests_TYPES_HPP
 #define orogen_syskit_tests_TYPES_HPP
 
-#include <stdint.h>
-
+#include <base/Time.hpp>
 
 namespace orogen_syskit_tests {
-    struct Time {
-        int64_t microseconds = 0;
-        int64_t tv_sec = 0;
-        int64_t tv_usec = 0;
-    };
-
     struct TestType {
         /** @meta role logical_time */
-        Time timestamp;
+        base::Time timestamp;
         int test_value = 0;
     };
 }
